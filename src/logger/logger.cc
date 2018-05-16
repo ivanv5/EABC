f80c244eca9d99172cd995c0dbbd8c69dbe8a041
Ivan Valbuena <ivan.valbuena@gmail.com>
2018-05-17 01:08:35 +0200

    measurent readings fixed

--- src/logger/logger.cc
+++ src/logger/logger.cc
@@ -8,19 +8,23 @@
 #include <cerrno>
 
 int main() {
-    hyt221 sensor_th;
+    hyt221 sensor_ht;
     bmp280 sensor_p;
     std::ofstream f("log.txt");
 
     for(;;) {
 	using namespace std::chrono_literals;
-	hyt221::measurement m = sensor_th.read_t_h();
+	hyt221::measurement m = sensor_ht.read_h_t();
 	float p = sensor_p.read_p();
 	std::time_t now = std::time(nullptr);
 	char ts[80];
-	std::strftime(ts, sizeof(ts), "%Y%m%d%H%M%S", std::localtime(&now));
-	f << ts << " " << m.t << " " << m.h << " " << p/100.f << std::endl;
-	std::cout << ts << " " << m.t << " " << m.h << " " << p/100.f << std::endl;
+	std::strftime(ts, sizeof(ts), "%d %m %Y %H:%M:%S", std::localtime(&now));
+	f << ts << " " << "T=" << m.t << "ºC" <<
+	 " " << "RH=" << m.h <<"%"<< " " << p/100.f 
+	<< "Pa"<< std::endl;
+	std::cout << ts << " " << "T=" << m.t << 
+	"ºC" << " " << "RH=" << m.h <<"%"<< " " << p/100.f
+	<< "Pa"<< std::endl;
 	f.flush();
 	std::this_thread::sleep_for(5s);
     }
